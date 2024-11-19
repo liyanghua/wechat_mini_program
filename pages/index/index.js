370e12eeb1b7e47804688dc8ea8773c042fd7d03
李阳华 <time.clifford@gmail.com>
2024-11-19 17:42:59 +0800

    add auto broadcast

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,347 +1,120 @@
 // pages/index/index.js
+const productsData = require('../../data/products.js')
+
 Page({
   data: {
-    products: [
-      {
-        "id": "646798296318",
-        "尺寸": "8寸/9寸/10",
-        "流行元素": "复古",
-        "扇面材质": "宣纸/绢布",
-        "打开扇面形状": "扇形",
-        "扇面工艺": "印刷/喷绘",
-        "扇骨数量": "14根/15根/18根",
-        "扇骨材质": "竹",
-        "desc": "定制广告扇",
-        sales: 100,
-        "适用人群": "大众",
-        "是否手工": "是",
-        "name": "来图定制广告扇子企业LOGO酒吧二维码中国风复古卡通宣纸折扇印刷",
-        "发货": "安徽宣城",
-        "skus": [
-          {
-            "sku": "宣纸1把定制【单价】",
-            "price": "7.76",
-            "timestamp": "2024-11-18 17:38:58",
-            "path": "/data_fetcher/data_646798296318/sku_images/01_宣纸1把定制【单价】.jpg"
-          },
-          {
-            "sku": "宣纸2-10把定制【单价】",
-            "price": "16.49",
-            "timestamp": "2024-11-18 17:39:00",
-            "path": "/data_fetcher/data_646798296318/sku_images/02_宣纸2-10把定制【单价】.jpg"
-          },
-          {
-            "sku": "宣纸20-50把定制【单价】",
-            "price": "14.55",
-            "timestamp": "2024-11-18 17:39:02",
-            "path": "/data_fetcher/data_646798296318/sku_images/03_宣纸20-50把定制【单价】.jpg"
-          },
-          {
-            "sku": "宣纸80-200把定制【单价】",
-            "price": "11.64",
-            "timestamp": "2024-11-18 17:39:03",
-            "path": "/data_fetcher/data_646798296318/sku_images/04_宣纸80-200把定制【单价】.jpg"
-          },
-          {
-            "sku": "宣纸300-500把定制【单价】",
-            "price": "10.67",
-            "timestamp": "2024-11-18 17:39:05",
-            "path": "/data_fetcher/data_646798296318/sku_images/05_宣纸300-500把定制【单价】.jpg"
-          },
-          {
-            "sku": "宣纸600-1000把定制【单价】",
-            "price": "9.7",
-            "timestamp": "2024-11-18 17:39:06",
-            "path": "/data_fetcher/data_646798296318/sku_images/06_宣纸600-1000把定制【单价】.jpg"
-          },
-          {
-            "sku": "宣纸1000把以上【单价】",
-            "price": "8.73",
-            "timestamp": "2024-11-18 17:39:08",
-            "path": "/data_fetcher/data_646798296318/sku_images/07_宣纸1000把以上【单价】.jpg"
-          },
-          {
-            "sku": "宣纸2000把以上【单价】",
-            "price": "8.25",
-            "timestamp": "2024-11-18 17:39:10",
-            "path": "/data_fetcher/data_646798296318/sku_images/08_宣纸2000把以上【单价】.jpg"
-          },
-          {
-            "sku": "宣纸20000以上【单价】",
-            "price": "7.76",
-            "timestamp": "2024-11-18 17:39:11",
-            "path": "/data_fetcher/data_646798296318/sku_images/09_宣纸20000以上【单价】.jpg"
-          },
-          {
-            "sku": "绢布1把定制【单价】",
-            "price": "25.22",
-            "timestamp": "2024-11-18 17:39:13",
-            "path": "/data_fetcher/data_646798296318/sku_images/10_绢布1把定制【单价】.jpg"
-          },
-          {
-            "sku": "绢布2-10把定制【单价】",
-            "price": "24.25",
-            "timestamp": "2024-11-18 17:39:15",
-            "path": "/data_fetcher/data_646798296318/sku_images/11_绢布2-10把定制【单价】.jpg"
-          },
-          {
-            "sku": "绢布20-50把定制【单价】",
-            "price": "17.46",
-            "timestamp": "2024-11-18 17:39:16",
-            "path": "/data_fetcher/data_646798296318/sku_images/12_绢布20-50把定制【单价】.jpg"
-          },
-          {
-            "sku": "绢布80-200把定制【单价】",
-            "price": "15.52",
-            "timestamp": "2024-11-18 17:39:18",
-            "path": "/data_fetcher/data_646798296318/sku_images/13_绢布80-200把定制【单价】.jpg"
-          },
-          {
-            "sku": "绢布300-500把定制【单价】",
-            "price": "13.58",
-            "timestamp": "2024-11-18 17:39:20",
-            "path": "/data_fetcher/data_646798296318/sku_images/14_绢布300-500把定制【单价】.jpg"
-          },
-          {
-            "sku": "绢布600-1000把定制【单价】",
-            "price": "12.61",
-            "timestamp": "2024-11-18 17:39:21",
-            "path": "/data_fetcher/data_646798296318/sku_images/15_绢布600-1000把定制【单价】.jpg"
-          },
-          {
-            "sku": "绢布1000把以上【单价】",
-            "price": "11.64",
-            "timestamp": "2024-11-18 17:39:23",
-            "path": "/data_fetcher/data_646798296318/sku_images/16_绢布1000把以上【单价】.jpg"
-          },
-          {
-            "sku": "绢布2000把以上【单价】",
-            "price": "10.67",
-            "timestamp": "2024-11-18 17:39:24",
-            "path": "/data_fetcher/data_646798296318/sku_images/17_绢布2000把以上【单价】.jpg"
-          },
-          {
-            "sku": "绢布5000把以上【单价】",
-            "price": "9.7",
-            "timestamp": "2024-11-18 17:39:26",
-            "path": "/data_fetcher/data_646798296318/sku_images/18_绢布5000把以上【单价】.jpg"
-          },
-          {
-            "sku": "定制广告扇",
-            "price": "7.76",
-            "timestamp": "2024-11-18 17:39:28",
-            "path": "/data_fetcher/data_646798296318/sku_images/01_宣纸1把定制【单价】.jpg"
-          }
-        ],
-        "originalImage": "/images/products/扇子/646798296318/原图.png",
-        "image":"/images/products/扇子/646798296318/原图.png",
-        "price": "7.76",
-        "maskImage": "/data_fetcher/data_646798296318/sku_images/mask.png",
-        "printArea": {
-          "x": 0.3,
-          "y": 0.2,
-          "width": 0.4,
-          "height": 0.4
-        },
-        "mediaInfo": {
-          "mainImages": [
-            "/data_fetcher/data_646798296318/main_images/4.jpg",
-            "/data_fetcher/data_646798296318/main_images/5.jpg",
-            "/data_fetcher/data_646798296318/main_images/2.jpg",
-            "/data_fetcher/data_646798296318/main_images/3.jpg",
-            "/data_fetcher/data_646798296318/main_images/1.jpg"
-          ],
-          "detailImages": [
-            "/data_fetcher/data_646798296318/detail_images/14.jpg",
-            "/data_fetcher/data_646798296318/detail_images/28.jpg",
-            "/data_fetcher/data_646798296318/detail_images/15.jpg",
-            "/data_fetcher/data_646798296318/detail_images/01.jpg",
-            "/data_fetcher/data_646798296318/detail_images/17.jpg",
-            "/data_fetcher/data_646798296318/detail_images/03.jpg",
-            "/data_fetcher/data_646798296318/detail_images/02.jpg",
-            "/data_fetcher/data_646798296318/detail_images/16.jpg",
-            "/data_fetcher/data_646798296318/detail_images/12.jpg",
-            "/data_fetcher/data_646798296318/detail_images/06.jpg",
-            "/data_fetcher/data_646798296318/detail_images/07.jpg",
-            "/data_fetcher/data_646798296318/detail_images/13.jpg",
-            "/data_fetcher/data_646798296318/detail_images/05.jpg",
-            "/data_fetcher/data_646798296318/detail_images/11.jpg",
-            "/data_fetcher/data_646798296318/detail_images/10.jpg",
-            "/data_fetcher/data_646798296318/detail_images/04.jpg",
-            "/data_fetcher/data_646798296318/detail_images/21.jpg",
-            "/data_fetcher/data_646798296318/detail_images/09.jpg",
-            "/data_fetcher/data_646798296318/detail_images/08.jpg",
-            "/data_fetcher/data_646798296318/detail_images/20.jpg",
-            "/data_fetcher/data_646798296318/detail_images/22.jpg",
-            "/data_fetcher/data_646798296318/detail_images/23.jpg",
-            "/data_fetcher/data_646798296318/detail_images/27.jpg",
-            "/data_fetcher/data_646798296318/detail_images/26.jpg",
-            "/data_fetcher/data_646798296318/detail_images/18.jpg",
-            "/data_fetcher/data_646798296318/detail_images/24.jpg",
-            "/data_fetcher/data_646798296318/detail_images/25.jpg",
-            "/data_fetcher/data_646798296318/detail_images/19.jpg"
-          ],
-          "skuImages": [
-            "/data_fetcher/data_646798296318/sku_images/01_宣纸1把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/03_宣纸20-50把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/02_宣纸2-10把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/09_宣纸20000以上【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/12_绢布20-50把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/10_绢布1把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/14_绢布300-500把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/18_绢布5000把以上【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/08_宣纸2000把以上【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/11_绢布2-10把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/04_宣纸80-200把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/15_绢布600-1000把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/06_宣纸600-1000把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/13_绢布80-200把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/16_绢布1000把以上【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/07_宣纸1000把以上【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/05_宣纸300-500把定制【单价】.jpg",
-            "/data_fetcher/data_646798296318/sku_images/17_绢布2000把以上【单价】.jpg"
-          ]
-        }
-      },
-      {
-        id: 1,
-        name: "定制相框",
-        desc: "定制永恒",
-        price: 99,
-        image: "/images/products/相框/原图.png",
-        originalImage: "/images/products/相框/原图.png",
-        maskImage: "/images/products/相框/蒙版.png",
-        printArea: {
-          x: 0.3,
-          y: 0.2,
-          width: 0.4,
-          height: 0.4
-        },
-        sales: 2341
-      },
-      {
-        id: 2,
-        name: "个性手机壳",
-        desc: "DIY照片定制 全包边保护",
-        price: 39.9,
-        image: "/images/products/手机壳/原图.png",
-        originalImage: "/images/products/手机壳/原图.png",
-        maskImage: "/images/products/手机壳/蒙版.png",
-        printArea: {
-          x: 0.3,
-          y: 0.2,
-          width: 0.4,
-          height: 0.4
-        },
-        sales: 1678
-      },
-      {
-        id: 3,
-        name: "拼图",
-        desc: "拼出你的精彩",
-        price: 49.9,
-        image: "/images/products/拼图/原图.png",
-        originalImage: "/images/products/拼图/原图.png",
-        maskImage: "/images/products/拼图/蒙版.png",
-        printArea: {
-          x: 0.3,
-          y: 0.2,
-          width: 0.4,
-          height: 0.4
-        },
-        sales: 999
-      },
-      {
-        id: 4,
-        name: "帆布包定制",
-        desc: "环保面料 大容量",
-        price: 79.9,
-        image: "/images/products/帆布包/原图.jpg",
-        originalImage: "/images/products/帆布包/原图.jpg",
-        maskImage: "/images/products/帆布包/蒙版.png",
-        printArea: {
-          x: 0.3,
-          y: 0.2,
-          width: 0.4,
-          height: 0.4
-        },
-        sales: 876
-      },
-      {
-        id: 5,
-        name: "扇子定制",
-        desc: "环保面料 大容量",
-        price: 79.9,
-        image: "/images/products/扇子/646798296318/原图.png",
-        originalImage: "/images/products/扇子/646798296318/原图.png",
-        maskImage: "/images/products/扇子/646798296318/蒙版.png",
-        printArea: {
-          x: 0.3,
-          y: 0.2,
-          width: 0.4,
-          height: 0.4
-        },
-        sales: 876
-      }
-    ],
-    loading: false,
+    products: [], // 处理后的商品数据
+    loading: true,
+    currentPage: 1,
+    pageSize: 3,
     hasMore: true,
-    page: 1
+    swiperDuration: 2000, // 轮播间隔，5秒
   },
 
   onLoad() {
-    // 初始化加载数据
-    this.loadData()
+    console.log('原始商品数据:', productsData)
+    // 处理商品数据，添加轮播相关属性
+    const processedProducts = productsData.map(product => this.processProductImages(product))
+    
+    this.setData({
+      allProducts: processedProducts, // 保存所有商品
+      hasMore: processedProducts.length > this.data.pageSize
+    }, () => {
+      this.loadNextPage() // 加载第一页
+    })
+
+    // 启动轮播定时器
+    this.startImageRotation()
   },
 
-  // 下拉刷新
-  onPullDownRefresh() {
-    this.setData({
-      products: [],
-      page: 1,
-      hasMore: true
-    }, () => {
-      this.loadData().then(() => {
-        wx.stopPullDownRefresh()
+  processProductImages(product) {
+    // 合并和处理图片数组
+    let images = []
+    
+    // 如果有 image 字段，放在第一位
+    if (product.image) {
+      images.push(product.image)
+    }
+    
+    // 添加 mainImages 中的图片
+    if (product.mediaInfo?.mainImages?.length) {
+      images = images.concat(product.mediaInfo.mainImages)
+    }
+    
+  
+    // 去重
+    images = Array.from(new Set(images))
+
+    return {
+      ...product,
+      displayPrice: `¥${product.skus?.[0]?.price || 29.9}`,
+      images: images,
+      currentImageIndex: 0, // 当前显示的图片索引
+    }
+  },
+
+  startImageRotation() {
+    // 清除可能存在的旧定时器
+    if (this.rotationTimer) {
+      clearInterval(this.rotationTimer)
+    }
+
+    // 创建新的定时器
+    this.rotationTimer = setInterval(() => {
+      const { products } = this.data
+      const updatedProducts = products.map(product => {
+        if (product.images.length > 1) {
+          return {
+            ...product,
+            currentImageIndex: (product.currentImageIndex + 1) % product.images.length
+          }
+        }
+        return product
       })
+
+      this.setData({ products: updatedProducts })
+    }, this.data.swiperDuration)
+  },
+
+  // 加载下一页数据
+  loadNextPage() {
+    const { currentPage, pageSize, allProducts } = this.data
+    const start = (currentPage - 1) * pageSize
+    const end = start + pageSize
+    const newProducts = allProducts.slice(start, end)
+
+    this.setData({
+      products: [...this.data.products, ...newProducts],
+      currentPage: currentPage + 1,
+      hasMore: end < allProducts.length,
+      loading: false
     })
   },
 
   // 上拉加载更多
   onReachBottom() {
     if (this.data.hasMore && !this.data.loading) {
-      this.loadData()
+      this.setData({ loading: true })
+      this.loadNextPage()
     }
   },
 
-  // 加载数据
-  async loadData() {
-    if (!this.data.hasMore || this.data.loading) return
-    
-    this.setData({ loading: true })
-    
-    try {
-      // 模拟API请求
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      
-      // 模拟新数据
-      const newProducts = this.data.products.map(item => ({
-        ...item,
-        id: item.id + this.data.products.length
-      }))
-      
-      this.setData({
-        products: [...this.data.products, ...newProducts],
-        page: this.data.page + 1,
-        hasMore: this.data.page < 4 // 模拟4页数据
-      })
-    } catch (error) {
-      wx.showToast({
-        title: '加载失败，请重试',
-        icon: 'none'
-      })
-    } finally {
-      this.setData({ loading: false })
+  // 页面隐藏时停止轮播
+  onHide() {
+    if (this.rotationTimer) {
+      clearInterval(this.rotationTimer)
+    }
+  },
+
+  // 页面显示时恢复轮播
+  onShow() {
+    this.startImageRotation()
+  },
+
+  // 页面卸载时清理
+  onUnload() {
+    if (this.rotationTimer) {
+      clearInterval(this.rotationTimer)
     }
   },
 
@@ -350,18 +123,31 @@ Page({
     const { id } = e.currentTarget.dataset
     const product = this.data.products.find(p => p.id === id)
     
-    // 将商品信息序列化后传递
-    const productInfo = JSON.stringify({
+    if (!product) {
+      wx.showToast({
+        title: '商品信息不存在',
+        icon: 'none'
+      })
+      return
+    }
+
+    const productInfo = {
       id: product.id,
       name: product.name,
-      price: product.price,
-      originalImage: product.originalImage,
+      originalImage: product.originalImage || product.images[0],
       maskImage: product.maskImage,
-      printArea: product.printArea
-    })
-    
+      printArea: product.printArea,
+      price: product.skus[0]?.price,
+      properties: {
+        title: product.name,
+        shipping: product.shipping,
+        material: product.material,
+        size: product.size
+      }
+    }
+
     wx.navigateTo({
-      url: `/pages/customize/customize?productInfo=${encodeURIComponent(productInfo)}`
+      url: `/pages/customize/customize?productInfo=${encodeURIComponent(JSON.stringify(productInfo))}`
     })
   }
 })
